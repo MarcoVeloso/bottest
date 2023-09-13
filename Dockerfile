b798fc9b7dd756ba280495c919b0a86c164b59b6
Marco Franklin dos Santos Veloso <marco.veloso@bcb.gov.br>
2023-09-13 15:55:40 -0300

    Custom action personalizado

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM rasa/rasa-sdk:1.10.0
 COPY actions /app/actions
 
 USER root
-RUN pip install --upgrade pip
+#RUN pip install --upgrade pip
 #RUN pip install --no-cache-dir -r /app/actions/requirements-actions.txt
 
 USER 1001
--- actions/actions.py
+++ actions/actions.py
@@ -3,42 +3,6 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-class ActionSpeakToAnAgent(Action):
-
-    def name(self) -> Text:
-        return "action_speak_to_an_agent"
-
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict]:
-
-        elementItem = {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Speak to our agent",
-                    "subtitle": "Nikki",
-                    "image_url": "https://the1webchat.azurewebsites.net/image.png",
-                    "buttons": [
-                        {
-                            "title": "call agent now",
-                            "url": "tel://+66819341789",
-                            "type": "web_url"
-                        }
-                    ]
-                }
-                ]
-            }
-        }
-        dispatcher.utter_message(attachment=elementItem)
-
-
-        return []
-
 class ActionHi(Action):
 
     def name(self) -> Text:
